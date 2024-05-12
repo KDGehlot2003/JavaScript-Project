03bedd874cafbc60384bdb2b0a944467891a9569
KDGehlot2003 <kdgehlot2003@gmail.com>
2024-05-12 11:12:59 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,8 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+button.forEach(function (button) {
+  button.addEventListener('click', function (ele) {
+    body.style.backgroundColor = ele.target.id;
+  });
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript-Project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/KDGehlot2003/JavaScript-Project)
